8f977a42b4373ab3a8eb8dc0b3bcba13444e6248
Elena Borisova <elenaborrisova@gmail.com>
2021-11-27 14:26:49 +0100

    add quick sort implementation

--- ds_algorithms/sorting/bubble_sort.py
+++ ds_algorithms/sorting/bubble_sort.py
@@ -34,6 +34,24 @@ def bubble_sort(arr):
     print("POST-OPTIMIZED ITERATION COUNT: {0}".format(iteration_count))
 
 
+# An optimized version of Bubble Sort
+def bubble_sort_optimized(arr):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+
 bubble_sort_unoptimized(nums.copy())
 bubble_sort(nums)
 print("POST SORT: {0}".format(nums))
+
+bubble_sort_optimized([1, 2, 3, 4, 5])
