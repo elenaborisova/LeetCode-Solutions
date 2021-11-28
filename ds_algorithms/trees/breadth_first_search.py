5266aaba636d6efc03a7a392d7d49ca0a665e3cc
Elena Borisova <elenaborrisova@gmail.com>
2021-11-28 19:03:13 +0100

    add trees and bfs

---
+++ ds_algorithms/trees/breadth_first_search.py
@@ -0,0 +1,54 @@
+from collections import deque
+from ds_algorithms.trees.tree import TreeNode
+
+
+def bfs(root_node, goal_value):
+    # initialize frontier queue
+    path_queue = deque()
+
+    # add root path to the frontier
+    initial_path = [root_node]
+    path_queue.appendleft(initial_path)
+
+    # search loop that continues as long as there are paths in the frontier
+    while path_queue:
+        # get the next path and node
+        # then output node value
+        current_path = path_queue.popleft()
+        current_node = current_path[-1]
+        print(f"Searching node with value: {current_node.value}")
+
+        # check if the goal node is found
+        if current_node.value == goal_value:
+            return current_path
+
+        # add paths to children to the frontier
+        for child in current_node.children:
+            new_path = current_path[:]
+            new_path.append(child)
+            path_queue.append(new_path)
+
+    # return an empty path if goal not found
+    return None
+
+
+sample_root_node = TreeNode("Home")
+docs = TreeNode("Documents")
+photos = TreeNode("Photos")
+sample_root_node.children = [docs, photos]
+my_wish = TreeNode("WishList.txt")
+my_todo = TreeNode("TodoList.txt")
+my_cat = TreeNode("Fluffy.jpg")
+my_dog = TreeNode("Spot.jpg")
+docs.children = [my_wish, my_todo]
+photos.children = [my_cat, my_dog]
+
+print(sample_root_node)
+
+goal_path = bfs(sample_root_node, "Fluffy.jpg")
+if goal_path is None:
+    print("No path found")
+else:
+    print("Path found")
+    for node in goal_path:
+        print(node.value)
