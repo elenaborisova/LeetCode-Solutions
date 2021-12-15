6662b1b651dd2afc22a268ab5e055b38d1593cb4
Elena Borisova <elenaborrisova@gmail.com>
2021-12-15 14:47:22 +0100

    Update knapsack_problem.py

--- ds_algorithms/dynamic_programming/knapsack_problem.py
+++ ds_algorithms/dynamic_programming/knapsack_problem.py
@@ -1,30 +1,54 @@
-def longest_common_subsequence(string_1, string_2):
-    print("Finding longest common subsequence of {0} and {1}".format(string_1, string_2))
-    grid = [[0 for col in range(len(string_1) + 1)] for row in range(len(string_2) + 1)]
-    for row in range(1, len(string_2) + 1):
-        print("Comparing: {0}".format(string_2[row - 1]))
-        for col in range(1, len(string_1) + 1):
-            print("Against: {0}".format(string_1[col - 1]))
-            if string_1[col - 1] == string_2[row - 1]:
-                grid[row][col] = grid[row - 1][col - 1] + 1
+# Time complexity: O(2 ^ n)
+def recursive_knapsack(weight_cap, weights, values, i):
+    if weight_cap == 0 or i == 0:
+        return 0
+    elif weights[i - 1] > weight_cap:
+        return recursive_knapsack(weight_cap, weights, values, i - 1)
+    else:
+        include_item = values[i - 1] + recursive_knapsack(weight_cap - weights[i - 1], weights, values, i - 1)
+
+        exclude_item = recursive_knapsack(weight_cap, weights, values, i - 1)
+
+        return max(include_item, exclude_item)
+
+
+# Time complexity: O(n * weight_cap)
+def dynamic_knapsack(weight_cap, weights, values):
+    rows = len(weights) + 1
+    cols = weight_cap + 1
+    matrix = [[] for _ in range(rows)]
+
+    for index in range(rows):
+        # Initialize columns for this row
+        matrix[index] = [-1 for _ in range(cols)]
+
+        for weight in range(cols):
+            if index == 0 or weight == 0:
+                matrix[index][weight] = 0
+            # If weight at previous row is less than or equal to current weight
+            elif weights[index - 1] <= weight:
+                # Calculate item to include
+                include_item = values[index - 1] + matrix[index - 1][weight - weights[index - 1]]
+
+                # Calculate item to exclude
+                exclude_item = matrix[index - 1][weight]
+
+                # Calculate the value of current cell
+                matrix[index][weight] = max(include_item, exclude_item)
             else:
-                grid[row][col] = max(grid[row - 1][col], grid[row][col - 1])
-
-    # construct subsequence
-    result = []
-    while row > 0 and col > 0:
-        if string_1[col - 1] == string_2[row - 1]:
-            result.append(string_1[col - 1])
-            row -= 1
-            col -= 1
-        elif grid[row - 1][col] > grid[row][col - 1]:
-            row -= 1
-        else:
-            col -= 1
-    result.reverse()
-    return "".join(result)
+                # Calculate the value of current cell
+                matrix[index][weight] = matrix[index - 1][weight]
+    # Return the value of the bottom right of matrix
+    return matrix[rows - 1][weight_cap]
 
 
-s1 = 'ACCGTT'
-s2 = 'CCAGCA'
-print(longest_common_subsequence(s1, s2))
+weight_cap = 50
+weights = [31, 10, 20, 19, 4, 3, 6]
+values = [70, 20, 39, 37, 7, 5, 10]
+print(dynamic_knapsack(weight_cap, weights, values))
+
+weight_cap = 5
+weights = [1, 3, 5]
+values = [250, 300, 500]
+i = len(values) - 1
+print(recursive_knapsack(weight_cap, weights, values, i))
